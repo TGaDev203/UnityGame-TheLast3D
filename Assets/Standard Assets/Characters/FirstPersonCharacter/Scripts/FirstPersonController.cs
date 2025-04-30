9c68a6513a0cdb02f0c24bcf291efdd5dd1d39f8
TGaDev <hoangthuychimte@gmail.com>
2025-04-30 12:46:25 +0700

    Initial commit

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -1,257 +1,177 @@
-using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
-using UnityStandardAssets.Utility;
-using Random = UnityEngine.Random;
 
 namespace UnityStandardAssets.Characters.FirstPerson
 {
-    [RequireComponent(typeof (CharacterController))]
-    [RequireComponent(typeof (AudioSource))]
     public class FirstPersonController : MonoBehaviour
     {
-        [SerializeField] private bool m_IsWalking;
+        [SerializeField] private float cameraSensitivity;
+        [SerializeField] private float moveInputDeadZone;
         [SerializeField] private float m_WalkSpeed;
         [SerializeField] private float m_RunSpeed;
-        [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
-        [SerializeField] private float m_JumpSpeed;
-        [SerializeField] private float m_StickToGroundForce;
-        [SerializeField] private float m_GravityMultiplier;
-        [SerializeField] private MouseLook m_MouseLook;
-        [SerializeField] private bool m_UseFovKick;
-        [SerializeField] private FOVKick m_FovKick = new FOVKick();
-        [SerializeField] private bool m_UseHeadBob;
-        [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
-        [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
-        [SerializeField] private float m_StepInterval;
-        [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
-        [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-        [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
-
-        private Camera m_Camera;
-        private bool m_Jump;
-        private float m_YRotation;
+        // [SerializeField] private AudioClip[] m_FootstepSounds;
+        // [SerializeField] private AudioClip m_JumpSound;
+        // [SerializeField] private AudioClip m_LandSound;
         private Vector2 m_Input;
-        private Vector3 m_MoveDir = Vector3.zero;
         private CharacterController m_CharacterController;
-        private CollisionFlags m_CollisionFlags;
-        private bool m_PreviouslyGrounded;
-        private Vector3 m_OriginalCameraPosition;
-        private float m_StepCycle;
-        private float m_NextStep;
-        private bool m_Jumping;
-        private AudioSource m_AudioSource;
+        private Vector2 moveTouchStartPosition;
+        [SerializeField] private Transform cameraTransform;
 
-        // Use this for initialization
+        // Touch detection
+        private int leftFingerId, rightFingerId;
+        private float halfScreenWidth;
+
+        // Camera controller
+        Vector2 lookInput;
+        float cameraPitch;
+
         private void Start()
         {
+            // Id = -1 means the finger is not being tracked
+            leftFingerId = -1;
+            rightFingerId = -1;
+
+            // Only calculate once
+            halfScreenWidth = Screen.width / 2;
+
             m_CharacterController = GetComponent<CharacterController>();
-            m_Camera = Camera.main;
-            m_OriginalCameraPosition = m_Camera.transform.localPosition;
-            m_FovKick.Setup(m_Camera);
-            m_HeadBob.Setup(m_Camera, m_StepInterval);
-            m_StepCycle = 0f;
-            m_NextStep = m_StepCycle/2f;
-            m_Jumping = false;
-            m_AudioSource = GetComponent<AudioSource>();
-			m_MouseLook.Init(transform , m_Camera.transform);
+
+            // Calculate the movement input dead zone
+            moveInputDeadZone = Mathf.Pow(Screen.height / moveInputDeadZone, 2);
         }
 
-
-        // Update is called once per frame
         private void Update()
         {
-            RotateView();
-            // the jump state needs to read here to make sure it is not missed
-            if (!m_Jump)
+            GetTouchInput();
+
+            if (rightFingerId != -1)
             {
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                // Only look around if the right finger is being tracked;
+                LookAround();
+                Debug.Log("Rotating");
             }
 
-            if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
+            if (leftFingerId != -1)
             {
-                StartCoroutine(m_JumpBob.DoBobCycle());
-                PlayLandingSound();
-                m_MoveDir.y = 0f;
-                m_Jumping = false;
+                Move();
+                Debug.Log("Moving");
             }
-            if (!m_CharacterController.isGrounded && !m_Jumping && m_PreviouslyGrounded)
-            {
-                m_MoveDir.y = 0f;
-            }
-
-            m_PreviouslyGrounded = m_CharacterController.isGrounded;
         }
 
-
-        private void PlayLandingSound()
+        private void GetTouchInput()
         {
-            m_AudioSource.clip = m_LandSound;
-            m_AudioSource.Play();
-            m_NextStep = m_StepCycle + .5f;
-        }
-
-
-        private void FixedUpdate()
-        {
-            float speed;
-            GetInput(out speed);
-            // always move along the camera forward as it is the direction that it being aimed at
-            Vector3 desiredMove = transform.forward*m_Input.y + transform.right*m_Input.x;
-
-            // get a normal for the surface that is being touched to move along it
-            RaycastHit hitInfo;
-            Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
-                               m_CharacterController.height/2f);
-            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
-
-            m_MoveDir.x = desiredMove.x*speed;
-            m_MoveDir.z = desiredMove.z*speed;
-
-
-            if (m_CharacterController.isGrounded)
+            // Iterate through all the detected touches
+            for (int i = 0; i < Input.touchCount; i++)
             {
-                m_MoveDir.y = -m_StickToGroundForce;
+                Touch touch = Input.GetTouch(i);
 
-                if (m_Jump)
+                // Check each touch's phase
+                switch (touch.phase)
                 {
-                    m_MoveDir.y = m_JumpSpeed;
-                    PlayJumpSound();
-                    m_Jump = false;
-                    m_Jumping = true;
+                    case TouchPhase.Began:
+                        if (touch.position.x < halfScreenWidth && leftFingerId == -1)
+                        {
+                            leftFingerId = touch.fingerId;
+                            Debug.Log("Tracking left finger");
+                            moveTouchStartPosition = touch.position;
+                        }
+                        else if (touch.position.x > halfScreenWidth && rightFingerId == -1)
+                        {
+                            rightFingerId = touch.fingerId;
+                        }
+
+                        break;
+
+                    case TouchPhase.Ended:
+                    case TouchPhase.Canceled:
+                        if (touch.fingerId == leftFingerId)
+                        {
+                            // Stop tracking the left finger
+                            leftFingerId = -1;
+                            Debug.Log("Stopped tracking left finger");
+                        }
+                        else if (touch.fingerId == rightFingerId)
+                        {
+                            // Stop tracking the right finger
+                            rightFingerId = -1;
+                            Debug.Log("Stop tracking right finger");
+                        }
+
+                        break;
+
+                    case TouchPhase.Moved:
+                        // Get input for looking around
+                        if (touch.fingerId == rightFingerId)
+                        {
+                            lookInput = touch.deltaPosition * cameraSensitivity * Time.deltaTime;
+                        }
+                        else if (touch.fingerId == leftFingerId)
+                        {
+                            // Caculating the position delta from the start position
+                            m_Input = touch.position - moveTouchStartPosition;
+                        }
+
+                        break;
+
+                    case TouchPhase.Stationary:
+                        // Set the look input to zero if the finger is still
+                        if (touch.fingerId == rightFingerId)
+                        {
+                            lookInput = Vector2.zero;
+                        }
+                        break;
                 }
             }
-            else
-            {
-                m_MoveDir += Physics.gravity*m_GravityMultiplier*Time.fixedDeltaTime;
-            }
-            m_CollisionFlags = m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
-
-            ProgressStepCycle(speed);
-            UpdateCameraPosition(speed);
         }
 
-
-        private void PlayJumpSound()
+        private void LookAround()
         {
-            m_AudioSource.clip = m_JumpSound;
-            m_AudioSource.Play();
+            // Vertial (pitch) rotation
+            cameraPitch = Mathf.Clamp(cameraPitch - lookInput.y, -90f, 90f);
+            cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
+
+            // Hoizontal (yaw) rotation
+            transform.Rotate(transform.up, lookInput.x);
         }
 
+        // private void PlayLandingSound()
+        // {
+        //     m_AudioSource.clip = m_LandSound;
+        //     m_AudioSource.Play();
+        // }
 
-        private void ProgressStepCycle(float speed)
+        // private void PlayJumpSound()
+        // {
+        //     m_AudioSource.clip = m_JumpSound;
+        //     m_AudioSource.Play();
+        // }
+
+        // private void PlayFootStepAudio()
+        // {
+        //     if (!m_CharacterController.isGrounded)
+        //     {
+        //         return;
+        //     }
+        //     // pick & play a random footstep sound from the array,
+        //     // excluding sound at index 0
+        //     int n = Random.Range(1, m_FootstepSounds.Length);
+        //     m_AudioSource.clip = m_FootstepSounds[n];
+        //     m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        //     // move picked sound to index 0 so it's not picked next time
+        //     m_FootstepSounds[n] = m_FootstepSounds[0];
+        //     m_FootstepSounds[0] = m_AudioSource.clip;
+        // }
+
+        private void Move()
         {
-            if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0))
-            {
-                m_StepCycle += (m_CharacterController.velocity.magnitude + (speed*(m_IsWalking ? 1f : m_RunstepLenghten)))*
-                             Time.fixedDeltaTime;
-            }
+            // Do not move if the touch delta is shorter than the designated dead zone
+            if (m_Input.sqrMagnitude <= moveInputDeadZone) return;
 
-            if (!(m_StepCycle > m_NextStep))
-            {
-                return;
-            }
+            // Multiply the normalized direction by the speed
+            Vector2 movementDirection = m_Input.normalized * m_WalkSpeed * Time.deltaTime;
 
-            m_NextStep = m_StepCycle + m_StepInterval;
-
-            PlayFootStepAudio();
-        }
-
-
-        private void PlayFootStepAudio()
-        {
-            if (!m_CharacterController.isGrounded)
-            {
-                return;
-            }
-            // pick & play a random footstep sound from the array,
-            // excluding sound at index 0
-            int n = Random.Range(1, m_FootstepSounds.Length);
-            m_AudioSource.clip = m_FootstepSounds[n];
-            m_AudioSource.PlayOneShot(m_AudioSource.clip);
-            // move picked sound to index 0 so it's not picked next time
-            m_FootstepSounds[n] = m_FootstepSounds[0];
-            m_FootstepSounds[0] = m_AudioSource.clip;
-        }
-
-
-        private void UpdateCameraPosition(float speed)
-        {
-            Vector3 newCameraPosition;
-            if (!m_UseHeadBob)
-            {
-                return;
-            }
-            if (m_CharacterController.velocity.magnitude > 0 && m_CharacterController.isGrounded)
-            {
-                m_Camera.transform.localPosition =
-                    m_HeadBob.DoHeadBob(m_CharacterController.velocity.magnitude +
-                                      (speed*(m_IsWalking ? 1f : m_RunstepLenghten)));
-                newCameraPosition = m_Camera.transform.localPosition;
-                newCameraPosition.y = m_Camera.transform.localPosition.y - m_JumpBob.Offset();
-            }
-            else
-            {
-                newCameraPosition = m_Camera.transform.localPosition;
-                newCameraPosition.y = m_OriginalCameraPosition.y - m_JumpBob.Offset();
-            }
-            m_Camera.transform.localPosition = newCameraPosition;
-        }
-
-
-        private void GetInput(out float speed)
-        {
-            // Read input
-            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-            float vertical = CrossPlatformInputManager.GetAxis("Vertical");
-
-            bool waswalking = m_IsWalking;
-
-#if !MOBILE_INPUT
-            // On standalone builds, walk/run speed is modified by a key press.
-            // keep track of whether or not the character is walking or running
-            m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
-#endif
-            // set the desired speed to be walking or running
-            speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
-            m_Input = new Vector2(horizontal, vertical);
-
-            // normalize input if it exceeds 1 in combined length:
-            if (m_Input.sqrMagnitude > 1)
-            {
-                m_Input.Normalize();
-            }
-
-            // handle speed change to give an fov kick
-            // only if the player is going to a run, is running and the fovkick is to be used
-            if (m_IsWalking != waswalking && m_UseFovKick && m_CharacterController.velocity.sqrMagnitude > 0)
-            {
-                StopAllCoroutines();
-                StartCoroutine(!m_IsWalking ? m_FovKick.FOVKickUp() : m_FovKick.FOVKickDown());
-            }
-        }
-
-
-        private void RotateView()
-        {
-            m_MouseLook.LookRotation (transform, m_Camera.transform);
-        }
-
-
-        private void OnControllerColliderHit(ControllerColliderHit hit)
-        {
-            Rigidbody body = hit.collider.attachedRigidbody;
-            //dont move the rigidbody if the character is on top of it
-            if (m_CollisionFlags == CollisionFlags.Below)
-            {
-                return;
-            }
-
-            if (body == null || body.isKinematic)
-            {
-                return;
-            }
-            body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
+            // Move relatively to the local transform's direction
+            m_CharacterController.Move(transform.right * movementDirection.x + transform.forward * movementDirection.y);
         }
     }
+
 }
